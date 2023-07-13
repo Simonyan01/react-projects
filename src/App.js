3658c4496629af2ed2ae77b71a612113ef815295
Simonyan <lyov.simonyan.01@gmail.com>
2023-07-13 16:38:53 +0400

    Success

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-// import { questions } from "./question";
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { questions } from "./question";
 import { Game } from "./components/Game";
-// import { Result } from "./components/Result";
+import { Result } from "./components/Result";
 import "./index.scss";
 
-export const App = () => {
+export default function App() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (i) => {
+    console.log(step, i);
+    setStep(step + 1);
+    if (i === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
-};
+}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { questions } from "../question";
 
+export const Game = ({ step, question, onClickVariant }) => {
+  const percentage = Math.round((step / questions.length) * 100)
 
-export const Game = () => {
   return (
     <>
       <div className="progress">
-        <div style={{ width: "50%" }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
