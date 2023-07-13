307122753a36ff7896aa6146eb3b886b97cc76f2
Simonyan <lyov.simonyan.01@gmail.com>
2023-07-13 15:36:28 +0400

    Success

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
-import React, { useState, useEffect } from "react";
+// import { questions } from "./question";
+import React from "react";
+import { Game } from "./components/Game";
+// import { Result } from "./components/Result";
 import "./index.scss";
-import { Success } from "./components/Success";
-import { Users } from "./components/Users/Main";
 
-const API = "https://reqres.in/api/users"
-
-export default function App() {
-  const [users, setUsers] = useState([]);
-  const [invites, setInvites] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [success, setSuccess] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-
-  useEffect(() => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((json) => setUsers(json.data))
-      .catch((err) => {
-        console.warn(err);
-        console.log("Ошибка при получении пользователей");
-      })
-      .finally(() => setLoading(false));
-  }, [])
-
-  const onChangeSearchValue = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  const onClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites((prev) => prev.filter((_id) => _id !== id));
-    } else {
-      setInvites((prev) => [...prev, id]);
-    }
-  };
-
-  const onClickSendInvites = () => {
-    setSuccess(true);
-  };
-
+export const App = () => {
   return (
     <div className="App">
-      {success ? (
-        <Success count={invites.length} />
-      ) : (
-        <Users
-          searchValue={searchValue}
-          items={users}
-          isLoading={isLoading}
-          onChangeSearchValue={onChangeSearchValue}
-          invites={invites}
-          onClickInvite={onClickInvite}
-          onClickSendInvites={onClickSendInvites}
-        />
-      )}
+      <Game />
+      {/* <Result /> */}
     </div>
   );
-}
+};
