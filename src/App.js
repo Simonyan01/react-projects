1853f09e8fdda439f4fc07129cdc6ebf0e434d57
Simonyan <lyov.simonyan.01@gmail.com>
2023-07-13 20:04:58 +0400

    fix main branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
       <Block value={0} currency="USD" />
     </div>
-  );
+  )
 }
 
 export default App;
