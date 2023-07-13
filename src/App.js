084a7db0534a6ec00fe72bf8dd0ef81c75b105ef
Simonyan <lyov.simonyan.01@gmail.com>
2023-07-13 17:20:51 +0400

    Success

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { questions } from "./question";
-import { Game } from "./components/Game";
-import { Result } from "./components/Result";
-import "./index.scss";
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
-export default function App() {
-  const [step, setStep] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const question = questions[step];
-
-  const onClickVariant = (i) => {
-    console.log(step, i);
-    setStep(step + 1);
-    if (i === question.correct) {
-      setCorrect(correct + 1);
-    }
-  };
-
+function App() {
   return (
     <div className="App">
-      {step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant} />
-      ) : (
-        <Result correct={correct} />
-      )}
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
+
+export default App;
