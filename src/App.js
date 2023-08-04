5b56ea3e5e6bd9689c3345f14272e22a9b7d097e
Simonyan <lyov.simonyan.01@gmail.com>
2023-08-04 23:40:30 +0400

    Success

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Collection } from "./Collection";
-import "./index.scss";
 import { view } from "./view";
+import "./index.scss";
 
 export default function App() {
   const [collections, setCollections] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [categoryId, setCategoryId] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [searchValue, setSearchValue] = useState("");
-  const [categoriId, setCategoriId] = useState(0);
+
+  const category = categoryId ? `category=${categoryId}` : "";
+  const url = `https://64b13c9e062767bc4825e54e.mockapi.io/photo_collections?page=${page}&limit=3&${category}`;
 
   useEffect(() => {
     setIsLoading(true);
-    const category = categoriId ? `category=${categoriId}` : "";
-    
-    fetch(
-      `https://64b13c9e062767bc4825e54e.mockapi.io/photos?page=${page}&limit=3&${category}`
-    )
+    fetch(url)
       .then((res) => res.json())
-      .then((json) => {
-        setCollections(json);
+      .then((data) => setCollections(data))
+      .catch((error) => {
+        console.error("Ошибка при получении данных", error);
       })
-      .catch((err) => {
-        console.warn(err);
-        console.log("Ошибка при получении данных");
-      })
-      .finally(() => setIsLoading(false));
-  }, [categoriId, page]);
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [url]);
 
   return (
-    <div className="App">
+    <>
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
           {view.map((obj, i) => (
             <li
-              onClick={() => setCategoriId(i)}
-              className={categoriId === i ? "active" : ""}
               key={obj.name}
+              className={categoryId === i ? "active" : ""}
+              onClick={() => setCategoryId(i)}
             >
               {obj.name}
             </li>
@@ -52,25 +50,30 @@ export default function App() {
       </div>
       <div className="content">
         {isLoading ? (
-          <h2>Идёт загрузка ...</h2>
+          <h2> Идёт загрузка ... </h2>
+        ) : collections.length > 0 ? (
+          collections
+            .filter((obj) =>
+              obj.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((obj, i) => (
+              <Collection key={i} name={obj.name} images={obj.photos} />
+            ))
         ) : (
-          collections.map((obj, index) => (
-            <Collection key={index} name={obj.name} images={obj.photos} />
-          ))
+          !isLoading && <h2> Не найден </h2>
         )}
       </div>
       <ul className="pagination">
-        {[
-          ...Array(5).map((_, i) => (
-            <li
-              onClick={() => setPage(i + 1)}
-              className={page === i + 1 ? "active" : ""}
-            >
-              {i + 1}
-            </li>
-          )),
-        ]}
+        {[...Array(5)].map((_, i) => (
+          <li
+            key={i}
+            onClick={() => setPage(i + 1)}
+            className={page === i + 1 ? "active" : undefined}
+          >
+            {i + 1}
+          </li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
